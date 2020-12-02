48e4a662cc29f9208cb23deb7de4d3d580dcc442
justinbosma <jbosma0@gmail.com>
2020-12-02 11:29:04 -0500

    integrated index action for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,8 @@
 class PortfoliosController < ApplicationController
+	#Want to list out number of items - list out all portfolio items
+	#Calling Model - making available to the view
+	def index
+		@portfolio_items = Portfolio.all
+	end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+	Blog.create!(
+		title: "My Blog Post #{blog}",
+		body: "Fart Fart Farty Fart fart Fart"
+	)
+end
+
+5.times do |skill|
+	Skill.create!(
+		title: "Rails #{skill}",
+		percent_utilized: 15
+	)
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+	Portfolio.create!(
+		title: "Portfolio Title #{portfolio_item}",
+		subtitle: "My farting farts",
+		body: "Man who farted in my fart?",
+		main_image: "https://place-hold.it/600x400",
+		thumb_image: "https://place-hold.it/350x200"
+	)
+end
