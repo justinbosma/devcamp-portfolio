2cd46de738619110bb00a8d2b420af23ed29e3e7
justinbosma <jbosma0@gmail.com>
2020-12-08 10:40:17 -0500

    implemented defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -8,4 +8,13 @@ class Portfolio < ApplicationRecord
 
 	#Same function as above - lambda function
 	scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+
+	#runs after .new is called
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.main_image ||= "https://place-hold.it/600x400"
+		self.thumb_image ||= "https://place-hold.it/350x200"
+
+	end
 end
